068f235d4145be75d51efdfb48e4dd5bc9db0b08
Tore Aurstad <tore.aurstad@hemit.no>
2022-08-23 00:19:47 +0200

    Getting nth word

--- SpanStringSplit.Test/SpanExtensionsSpec.cs
+++ SpanStringSplit.Test/SpanExtensionsSpec.cs
@@ -21,5 +21,22 @@ namespace SpanStringSplit.Test
             CollectionAssert.AreEqual(new string[] { "The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog" }, tokens);
         }
 
+        [Test]
+        [TestCase(",, The, quick, brown, fox, jumped, over, the, lazy,, dog", 5, "fox")]
+        [TestCase(",, The, quick, brown, fox, jumped, over, the, lazy,, dog", 0, "")]
+        [TestCase(",, The, quick, brown, fox, jumped, over, the, lazy,, dog", 1, "")]
+        [TestCase(",, The, quick, brown, fox, jumped, over, the, lazy,, dog", 2, "The")]
+        [TestCase(",, The, quick, brown, fox, jumped, over, the, lazy,, dog", 3, "quick")]
+        [TestCase(",, The, quick, brown, fox, jumped, over, the, lazy,, dog", 7, "over")]
+        [TestCase(",, The, quick, brown, fox, jumped, over, the, lazy,, dog", 11, "dog")]
+        [TestCase(",, The, quick, brown, fox, jumped, over, the, lazy,, dog", 12, null)]
+        [TestCase(",, The, quick, brown, fox, jumped, over, the, lazy,, dog", 13, null)]
+        public void GetNthWord(string input, int nthWord, string expectedWord)
+        {
+            ReadOnlySpan<char> s = ",,The,quick,brown,fox,jumped,over,the,lazy,,dog".ToCharArray();
+            var word = s.GetNthToken(',', nthWord);
+            Assert.AreEqual(word, expectedWord); 
+        }
+
     }
 }
--- SpanStringSplit/SpanExtensions.cs
+++ SpanStringSplit/SpanExtensions.cs
@@ -16,6 +16,82 @@ namespace SpanStringSplit
             return tokens; 
         }
 
+        public static string GetNthToken(this ReadOnlySpan<char> inputSpan, char splitChar, int nthToken)
+        {
+            if (inputSpan == null)
+            {
+                return null;
+            }
+            int[] splitIndexes = inputSpan.SplitIndexes(splitChar, nthToken); 
+            if (splitIndexes.Length == 0)
+            {
+                return inputSpan.ToString();
+            }
+            if (nthToken == 0 && splitIndexes.Length > 0)
+            {
+                return inputSpan.Slice(0, splitIndexes[0]).ToString(); 
+            }
+            if (nthToken > splitIndexes.Length)
+            {
+                return null; 
+            }
+            if (nthToken == splitIndexes.Length)
+            {
+                var split = inputSpan.Slice(splitIndexes[nthToken-1]+1).ToString();
+                return split; 
+            }
+            if (nthToken <= splitIndexes.Length + 1)
+            {
+                var split = inputSpan.Slice(splitIndexes[nthToken-1]+1, splitIndexes[nthToken] - splitIndexes[nthToken-1]-1).ToString();
+                return split; 
+            }
+            return null; 
+
+        }
+
+        public static int[] SplitIndexes(this ReadOnlySpan<char> inputSpan, char splitChar,
+            int? highestSplitIndex = null)
+        {
+            if (inputSpan == null)
+            {
+                return Array.Empty<int>();
+            }
+            if (inputSpan.Length == 0)
+            {
+                return Array.Empty<int>();
+            }
+            bool isSplitCharFound = false;
+            foreach (char letter in inputSpan)
+            {
+                if (letter == splitChar)
+                {
+                    isSplitCharFound = true;
+                    break;
+                }
+            }
+            if (!isSplitCharFound)
+            {
+                return Array.Empty<int>();
+            }
+         
+            var splitIndexes = new List<int>();
+            var tokens = new List<string>();
+            int charIndex = 0;
+            foreach (var ch in inputSpan)
+            {
+                if (ch == splitChar)
+                {
+                    if (highestSplitIndex.HasValue && highestSplitIndex + 1 < splitIndexes.Count)
+                    {
+                        break; 
+                    }
+                    splitIndexes.Add(charIndex);
+                }
+                charIndex++; 
+            }
+            return splitIndexes.ToArray(); 
+        }
+
         public static string[] SplitSpan(this ReadOnlySpan<char> inputSpan, char splitChar, StringSplitOptions splitOptions)
         {
             if (inputSpan == null)
