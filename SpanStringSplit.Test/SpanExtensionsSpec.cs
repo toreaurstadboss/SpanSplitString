cc725f3fdf14836af50e53e3f89fe173fd1e0c1f
Tore Aurstad <tore.aurstad@hemit.no>
2022-08-22 22:44:50 +0200

    init commit

---
+++ SpanStringSplit.Test/SpanExtensionsSpec.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SpanStringSplit.Test
+{
+    [TestFixture]
+    public class SpanExtensionsSpec
+    {
+        [Test]
+        public void SplitStringsViaSpan()
+        {
+            var tokens = ",,The,quick,brown,fox,jumped,over,the,lazy,,dog".SplitViaSpan(',', StringSplitOptions.RemoveEmptyEntries);
+            CollectionAssert.AreEqual(new string[] { "The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog" }, tokens);
+        }
+
+        [Test]
+        public void SplitStringsUsingSpan()
+        {
+            ReadOnlySpan<char> s = ",,The,quick,brown,fox,jumped,over,the,lazy,,dog".ToCharArray();                
+            var tokens = s.SplitSpan(',', StringSplitOptions.RemoveEmptyEntries);
+            CollectionAssert.AreEqual(new string[] { "The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog" }, tokens);
+        }
+
+    }
+}
---
+++ SpanStringSplit/SpanExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpanStringSplit
+{
+    public static class SpanExtensions
+    {
+
+        public static string[] SplitViaSpan(this string input, char splitChar, StringSplitOptions splitOptions)
+        {
+            if (string.IsNullOrWhiteSpace(input) || input.IndexOf(splitChar) < 0)
+            {
+                return new string[] { input };
+            }
+            var tokens = SplitSpan(input.AsSpan(), splitChar, splitOptions);
+            return tokens; 
+
+        }
+
+        public static string[] SplitSpan(this ReadOnlySpan<char> inputSpan, char splitChar, StringSplitOptions splitOptions)
+        {
+            if (inputSpan == null)
+            {
+                return new string[] { null };
+            }
+            if (inputSpan.Length == 0)
+            {
+                return splitOptions == StringSplitOptions.None ? new string[] { string.Empty } : new string[0]; 
+            }
+            if (inputSpan.ToString().IndexOf(splitChar) < 0)
+            {
+                return new string[] { inputSpan.ToString() }; 
+            }
+
+            bool IsTokenToBeAdded(string token) => !string.IsNullOrWhiteSpace(token) || splitOptions == StringSplitOptions.None;
+
+            var splitIndexes = new List<int>();
+            var tokens = new List<string>();
+            int charIndx = 0;
+            foreach (var ch in inputSpan)
+            {
+                if (ch == splitChar)
+                {
+                    splitIndexes.Add(charIndx);
+                }
+                charIndx++;
+            }
+            int currentSplitIndex = 0;
+            foreach (var indx in splitIndexes)
+            {
+                if (currentSplitIndex == 0)
+                {
+                    string firstToken = inputSpan.Slice(0, splitIndexes[0]).ToString();
+                    if (IsTokenToBeAdded(firstToken))
+                    {
+                        tokens.Add(firstToken);
+                    }
+                }
+                else if (currentSplitIndex <= splitIndexes.Count)
+                {
+                    string intermediateToken = inputSpan.Slice(splitIndexes[currentSplitIndex - 1] + 1, splitIndexes[currentSplitIndex] - splitIndexes[currentSplitIndex - 1] - 1).ToString();
+                    if (IsTokenToBeAdded(intermediateToken))
+                    {
+                        tokens.Add(intermediateToken);
+                    }
+                }
+                currentSplitIndex++;
+            }
+            string lastToken = inputSpan.Slice(splitIndexes[currentSplitIndex - 1] + 1).ToString();
+            if (IsTokenToBeAdded(lastToken))
+            {
+                tokens.Add(lastToken);
+            }
+            return tokens.ToArray();
+        }
+
+    }
+}
